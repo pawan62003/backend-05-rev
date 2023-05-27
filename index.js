f0ff13f36c647154dacd652d09c39cdc7226f083
pawan kumar <pawan6200327812@gmail.com>
2023-05-27 12:13:02 +0530

    do

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ server.use(router)
 const PORT = 8000
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
+  console.log(`JSON Server is running on${PORT}`)
 })
